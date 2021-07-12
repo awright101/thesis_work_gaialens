bc38585b67ab0a74587ce36a1f973b639f61ff22
Austin Wright <austin.wright4@gmail.com>
2021-07-12 12:13:56 +0100

    commit message

--- models/simple_VAE.py
+++ models/simple_VAE.py
@@ -44,31 +44,39 @@ device = torch.device("cpu")
 
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self,num_feats,latent_dims):
         super(VAE, self).__init__()
-
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.input_dim = num_feats
+        self.fc1 = nn.Linear(self.input_dim, 400)
+        self.fc11 = nn.Linear(400, 300)
+        self.fc12 = nn.Linear(300, 200)
+        self.fc21 = nn.Linear(200, latent_dims)
+        self.fc22 = nn.Linear(200, latent_dims)
+        self.fc3 = nn.Linear(latent_dims, 200)
+        self.fc31 = nn.Linear(200, 300)
+        self.fc32 = nn.Linear(300, 400)
+        self.fc4 = nn.Linear(400, self.input_dim)
 
     def encode(self, x):
         h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        h2 = F.relu(self.fc11(h1))
+        h3 = F.relu(self.fc12(h2))
+        return self.fc21(h3), self.fc22(h3)
 
-    def reparameterize(self, mu, logvar):
+    def reparam(self, mu, logvar):
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
         return mu + eps*std
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        h4 = F.relu(self.fc31(h3))
+        h5 = F.relu(self.fc32(h4))
+        return torch.sigmoid(self.fc4(h5))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
+        mu, logvar = self.encode(x.view(-1, self.input_dim))
+        z = self.reparam(mu, logvar)
         return self.decode(z), mu, logvar
 
 
@@ -78,61 +86,21 @@ class VAE(nn.Module):
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1,14), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
+    KLD /= x.view(-1, 14).data.shape[0] * 14
+    
     return BCE + KLD
 
 
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+    
 
 
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-
-if __name__ == "__main__":
-
-    num_epochs = 10 
-
-    np.loadtxt('../data/wine2.csv',delimiter=',')
-    #for epoch in range(1, num_epochs+ 1):
 
   
 
