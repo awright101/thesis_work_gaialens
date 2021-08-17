85980e3bee8e5b97a138b0924c16487dce24e0c1
Austin Wright <austin.wright4@gmail.com>
2021-08-17 11:52:32 +0100

    update

--- models/simple_VAE.py
+++ models/simple_VAE.py
@@ -86,14 +86,14 @@ class VAE(nn.Module):
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1,14), reduction='sum')
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1,232), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    KLD /= x.view(-1, 14).data.shape[0] * 14
+    KLD /= x.view(-1, 14).data.shape[0] * 232
    
     
     return BCE + KLD
